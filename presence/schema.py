e7235df640262fc11360563885a59ef38863b4ad
zeke1806 <joeloliviersidy@gmail.com>
2019-12-13 15:26:41 +0300

    Ajout resolver categories

--- presence/schema.py
+++ presence/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 
-from .models import Individu, Etudiant
+from .models import Individu, Etudiant, Categorie
 
 # Type definition
 
@@ -15,11 +15,20 @@ class EtudiantType(DjangoObjectType):
     class Meta:
         model = Etudiant
 
+
+class CategorieType(DjangoObjectType):
+    class Meta:
+        model = Categorie
+
 # Query definition
 
 
 class Query(graphene.ObjectType):
     individus = graphene.List(IndividuType)
+    categories = graphene.List(CategorieType)
 
     def resolve_individus(self, info):
         return Individu.objects.all()
+
+    def resolve_categories(self, info):
+        return Categorie.objects.all()
