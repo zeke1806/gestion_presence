100ce26e375e17541bdebfbc9bc56365deaed32a
zeke1806 <joeloliviersidy@gmail.com>
2020-01-03 12:43:58 +0300

    Ajout mutation CreateEvent

--- presence/admin.py
+++ presence/admin.py
@@ -6,7 +6,7 @@ from .models import Individu, Responsable, Etudiant, GroupeParticipant, Apparten
 
 
 class EtudiantAdmin(admin.ModelAdmin):
-    list_display = ('individu', 'num_matricule', 'niveau', 'parcours')
+    list_display = ('id', 'individu', 'num_matricule', 'niveau', 'parcours')
 
 
 class EvenementAdmin(admin.ModelAdmin):
@@ -25,11 +25,19 @@ class ResponsableAdmin(admin.ModelAdmin):
     list_display = ('id', 'individu')
 
 
+class MatiereAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom_matiere')
+
+
+class GroupeParticipantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nom_groupe_participant')
+
+
 admin.site.register(Individu, IndividuAdmin)
 admin.site.register(Responsable, ResponsableAdmin)
 admin.site.register(Etudiant, EtudiantAdmin)
-admin.site.register(GroupeParticipant)
+admin.site.register(GroupeParticipant, GroupeParticipantAdmin)
 admin.site.register(Appartenir)
 admin.site.register(Categorie, CategorieAdmin)
-admin.site.register(Matiere)
+admin.site.register(Matiere, MatiereAdmin)
 admin.site.register(Evenement, EvenementAdmin)
